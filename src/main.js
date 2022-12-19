64917d2adc4126378f8963219b93699492aa39c9
Pavel16rus <kryuchkov.p@gmail.com>
2022-12-19 20:52:23 +0200

    1

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { TemperatureList } from "../ui/TemperatureList.js";
 // let latitude = 31.046;
 // let longitude=34.851;
 // let start_date="2022-12-18";
-// let end_date="2022-12-18";
+// let end_date="2022-12-18";   
 // const baseUrl = "https://api.open-meteo.com/v1/gfs?";
 // const baseParams = "&hourly=temperature_2m&timezone=IST&";
 // const url = `${baseUrl}latitude=${latitude}&longitude=${longitude}${baseParams}start_date=${start_date}&end_date=${end_date}`
@@ -13,10 +13,11 @@ import { TemperatureList } from "../ui/TemperatureList.js";
 // let promiseData = promiseResponse.then(response => response.json());
 // let dataProcessing = promiseData.then(data => console.log(data.hourly.temperature_2m));
 
-const params = {/*requered params for form*/};
+const params = {idForm: "weather_form", idDateFrom: "date_from_field", idDateTo: "date_to_field",
+idHoursFrom: "hour_from", idHoursTo: "hour_to", idError: "hours_error"};
 const weatherProcessor = new WeatherDataProcessor();
 const dataForm = new DataForm(params);
-const temperatureList = new TemperatureList("idList");
+const temperatureList = new TemperatureList("results_list");
 dataForm.addHandler((dataFromForm) => {
     const promiseData = weatherProcessor.getData(dataFromForm);
     promiseData.then(data => temperatureList.showTemperatures(data));
